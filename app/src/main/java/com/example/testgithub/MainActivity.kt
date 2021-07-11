e4fd28aa699e4c321dbd990bbec18a970efcbd70
VanNguyen98 <vannh98.dev@gmail.com>
2021-07-11 23:51:08 +0700

    first commit

--- app/src/main/java/com/example/testgithub/MainActivity.kt
+++ app/src/main/java/com/example/testgithub/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var a: String = ""
+        var a: String = "123"
     }
 }
